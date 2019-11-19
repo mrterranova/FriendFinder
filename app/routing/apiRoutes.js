4ec09632d5ccdbc2f35f809ce5497fa8f62f3468
Michal Terranova <michalterranova@mail.com>
2019-11-19 02:56:25 -0500

    styling

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,32 +6,32 @@ module.exports = function(app) {
 
 //GET for JSON only
   app.get("/api/friends", function(req, res) {
-      res.json(friends[i])
+    res.json(friends)
   });
 
 //POST requests
   app.post("/api/friends", function(req, res) {
       let userScore = req.body.scores;
-      let allScores = [];
-      var matched = 0;
-      var difference = 0;
+      let scoresArr = [];
+      let bestMatch = 0;
 
       //go through the array of friends
       for (var i=0; i< friends.length; i++) {
+        var scoreDiff = 0;
         for (var j=0; j<userScore.length; j++){
-          difference += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScore[j]));
+          scoreDiff += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScore[j]));
         }
-        allScores.push(difference);
+        scoresArr.push(scoreDiff);
       }
       //find the friend that matches
-      for (var i=0; i<allScores.length; i ++){
-        if (allScores[i] == allScores[matched]) {
-        matched = i;
-        } else if (allScores[i] <= allScores[matched]){
-            matched = i;
+      for (var i=0; i<scoresArr.length; i ++){
+        if (scoresArr[i] == scoresArr[bestMatch]) {
+            bestMatch = i;
+        } else if (scoresArr[i] <= scoresArr[bestMatch]){
+            bestMatch = i;
         }
       }
-      let bestFriend = friends[matched];
+      let bestFriend = friends[bestMatch];
       res.json(bestFriend);
 
       //add name to the array of friends
